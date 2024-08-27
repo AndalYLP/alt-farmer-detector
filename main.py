93d2d537760d44a5f0c827ae1e878277b62c51fb
AndalYLP <andal089.az@gmail.com>
2024-08-27 17:29:55 -0500

    Fixed lobby status in track times messages

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ async def UserStatus(userPresences, channel, AltChannel, todelete, todelete2):
             if GameId and not GameIdList.get(doc["userId"])[0][1] == GameId:
                 if Tracking.get(Username):
                     Result = int(time.time()) - int(GameIdList.get(doc["userId"])[1][1][3:-3])
-                    embed = discord.Embed(color=46847,title=f"Time in game: {(str(Result) + "Seconds") if Result < 60 else (str(int(Result / 60)) + ":" + str(Result % 60) + "Minutes" ) }", description=f"Game: **{GameName}**\nGameId: **{GameId}**\nLobby: **{LobbyStatus}**")
+                    embed = discord.Embed(color=46847,title=f"Time in game: {(str(Result) + "Seconds") if Result < 60 else (str(int(Result / 60)) + ":" + str(Result % 60) + "Minutes" ) }", description=f"Game: **{GameName}**\nGameId: **{GameId}**\nLobby: **{GameIdList.get(doc["userId"])[2]}**")
                     await Tracking[Username].send(embed=embed)
                 GameIdList.get(doc["userId"])[2] = "True" if doc["placeId"] == 6872265039 else "False"
                 GameIdList.get(doc["userId"])[1][0] = GameIdList.get(doc["userId"])[1][1]
