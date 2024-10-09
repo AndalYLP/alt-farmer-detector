65e2a7c5d9990b5d502819845fd585381dde66fd
AndalYLP <andal089.az@gmail.com>
2024-10-09 18:15:24 -0500

    f

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def UserStatus(userPresences, channel, AltChannel):
         
         results = list(UsersCollection.find({"UserID": {"$in": userIDs}}))
         for presence in userPresences:
-            Data = (doc for doc in results if doc["UserID"] == presence["userId"])
+            Data = list(doc for doc in results if doc["UserID"] == presence["userId"])
             ud = UserPresence(Data["Username"], presence["userId"], Data["isAlt"], presence["userPresenceType"], "True" if presence["placeId"] == 6872265039 else "False", presence["lastLocation"] or "None", presence["gameId"], Data.get("GroupName", "None"))
 
             if ud.UserID not in GameIdList:
