423b317ae3a4aae8d27d7971b14ca084df79c8ce
AndalYLP <andal089.az@gmail.com>
2024-10-12 10:39:41 -0500

    f

--- Commands/SnipeCommands.py
+++ Commands/SnipeCommands.py
@@ -13,42 +13,84 @@ COOKIE = os.environ.get("COOKIE")
 
 # --------------------------------- Functions -------------------------------- #
 
-Credits = 3
+Debounce = False
 TokensTotal = {}
 TokensTime = None
-notFound = True
 busy = False
-def tokenFormat(token):
-    return {
-        "requestId": f"0:{token}:AvatarHeadshot:48x48:png:regular",
-        "type": "AvatarHeadShot",
-        "targetId": 0,
-        "format": "png",
-        "size": "48x48",
-        "token": token,
-    }
 
-async def GetData(session, Data, i, image, Tokens, interaction: discord.Interaction, Username, Forced):
-    global notFound, TokensTime
-    async with session.post("https://thumbnails.roblox.com/v1/batch",json=Data) as response:
-        if response.status == 200:
-            data = await response.json()
-            thing = {data["imageUrl"]: data["requestId"].split(":")[1] for data in data["data"]}
-            if image in thing.keys():
-                notFound = False
-                color = 2686720
-                title = f"Found {Username}'s server!"
-                description = f"Game: **Bedwars** (yes.)\nLobby: **True** (yes.)\nGameId: **{Tokens[thing[image]]}**" 
-                embed = discord.Embed(color=color,title=title,description=description)
-                await interaction.followup.send(content=f"<t:{int(time.time())}:R>" + (f"Tokens Time: <t:{TokensTime}:R>" if Forced else ""),embed=embed)
-            return
+class AvatarFetcher:
+    def __init__(self, user_id, username, interaction):
+        self.user_id = user_id
+        self.image_url = None
+        self.found = False
+        self.username = username
+        self.interaction:discord.Interaction = interaction
 
-async def CheckImage(Tokens, image, interaction, Username, Forced):
-    TokenSubLists = [[tokenFormat(token) for token in list(Tokens.keys())[i:i + 100]] for i in range(0, len(Tokens.keys()), 100)]
+    async def fetch_image_url(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://thumbnails.roblox.com/v1/users/avatar-headshot?size=48x48&format=png&userIds={self.user_id}") as response:
+                if response.status == 200:
+                    self.image_url = (await response.json())["data"][0]["imageUrl"]
+                else:
+                    await self.interaction.followup.send("Request status code isn't 200 (Thumbnail API).")
 
-    async with aiohttp.ClientSession() as session:
-        tasks = [GetData(session, TokenSubList, i, image, Tokens, interaction, Username, Forced) for i, TokenSubList in enumerate(TokenSubLists)]
-        await asyncio.gather(*tasks)
+    async def fetch_batch_data(self, session, data, Force):
+        global busy
+        async with session.post("https://thumbnails.roblox.com/v1/batch", json=data) as response:
+            if response.status == 200:
+                result = await response.json()
+                found_data = {item["imageUrl"]: item["requestId"].split(":")[1] for item in result["data"]}
+                
+                if self.found:
+                    return
+                
+                if self.image_url in found_data:
+                    self.found = True
+                    busy = False
+                    color = 2686720
+                    title = f"Found {self.username}'s server!"
+                    description = f"Game: **Bedwars** (yes.)\nLobby: **True** (yes.)\nGameId: **{found_data[self.image_url]}**" 
+                    embed = discord.Embed(color=color,title=title,description=description)
+                    await self.interaction.followup.send(content=f"<t:{int(time.time())}:R>" + (f"Data from:<t:{int(TokensTime)}:R>" if Force else ""),embed=embed)
+
+    async def check_images(self, tokens):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for i in range(0, len(tokens), 100):
+                token_batch = [self.token_format(token) for token in list(tokens.keys())[i:i + 100]]
+                tasks.append(self.fetch_batch_data(session, token_batch, False))
+            await asyncio.gather(*tasks)
+
+    @staticmethod
+    def token_format(token):
+        return {
+            "requestId": f"0:{token}:AvatarHeadshot:48x48:png:regular",
+            "type": "AvatarHeadShot",
+            "targetId": 0,
+            "format": "png",
+            "size": "48x48",
+            "token": token,
+        }
+
+    async def run(self):
+        global busy, TokensTotal, TokensTime
+        busy = True
+        await self.fetch_image_url()
+        next_cursor = None
+
+        async with aiohttp.ClientSession() as session:
+            while not self.found:
+                response = await session.get(f"https://games.roblox.com/v1/games/6872265039/servers/public?limit=100" + (f"&cursor={next_cursor}" if next_cursor else ""))
+                if response.status == 200:
+                    response_json = await response.json()
+                    tokens = {token: server["id"] for server in response_json["data"] for token in server["playerTokens"]}
+                    TokensTotal.update(tokens)
+                    TokensTime = time.time()
+                    await self.check_images(tokens)
+                    next_cursor = response_json.get("nextPageCursor")
+                else:
+                    await self.interaction.followup.send("Request status code isn't 200 (Games API), waiting 60 seconds.", ephemeral=True)
+                    await asyncio.sleep(60)
 
 # ------------------------------------ Cog ----------------------------------- #
 
@@ -108,12 +150,12 @@ class SnipeCommands(commands.Cog):
     @joinsOffGroup.command(name="player",description="Send player status, only works with bedwars.")
     @app_commands.describe(username="Player username to snipe.", forceupdate="If true you will get the latest data and update the current data, if false you will search through the current data")
     async def joinsOffPlayer(self, interaction: discord.Interaction, username:str, forceupdate:bool):
-        global Credits, TokensTotal, notFound, TokensTime, busy
+        global Debounce, TokensTotal, TokensTime, busy
         if busy:
             await interaction.response.send_message("Im busy rn!.", delete_after=3, ephemeral=True)
         
-        if Credits != 3 and forceupdate:
-            await interaction.response.send_message("On cooldown, pls wait. " + str(Credits), delete_after=3, ephemeral=True)
+        if not Debounce and forceupdate:
+            await interaction.response.send_message("On cooldown, pls wait.", delete_after=3, ephemeral=True)
         print(interaction.user.name + " Used snipe player command")
 
         await interaction.response.defer(thinking=True)
@@ -126,59 +168,17 @@ class SnipeCommands(commands.Cog):
                 id = data[0].get("id")
                 Username = data[0].get("name")
 
-                response = requests.get(f"https://thumbnails.roblox.com/v1/users/avatar-headshot?size=48x48&format=png&userIds={id}")
-                if response.status_code == 200:
-                    responseJSON = response.json()
-
-                    data = responseJSON.get("data", [])
-                    if data and "imageUrl" in data[0]:
-                        image = data[0]["imageUrl"]
-                        if not forceupdate and TokensTotal:
-                            await CheckImage(TokensTotal, image, interaction, Username, forceupdate)
-                            return
-
-                        notFound = True
-                        nextCursor = None
-                        busy = True
-                        while notFound or Credits != 0:
-                            response = requests.get("https://games.roblox.com/v1/games/6872265039/servers/public?limit=100" + (f"&cursor={nextCursor}" if nextCursor else ""))
-                            if response.status_code == 200:
-                                responseJSON = response.json()
-
-                                Tokens = {token: server["id"] for server in responseJSON["data"] for token in server["playerTokens"]}
-                                TokensTotal.update(Tokens)
-                                TokensTime = int(time.time())
-                                await CheckImage(Tokens, image, interaction,Username, forceupdate)
-                                nextCursor = responseJSON["nextPageCursor"]
-                                Credits -= 1
-                                if Credits == 0:
-                                    if notFound:
-                                        await interaction.followup.send("Sent 3 requests, waiting 60 seconds.")
-                                        await asyncio.sleep(60)
-                                        Credits = 3
-                                        continue
-                                    else:
-                                        await asyncio.sleep(60)
-                                        Credits = 3
-                                        break
-                                    
-                                
-                                if not nextCursor:
-                                    if notFound:
-                                        await interaction.followup.send("No servers left, player not found.")
-                                    break
-                            else:
-                                await interaction.followup.send("Request status code isn't 200 (Games API).", ephemeral=True)
-                                await asyncio.sleep(30) 
-                        await interaction.followup.send("Finished.")
-                        busy = False
-                        if Credits != 3:
-                            await asyncio.sleep(60)
-                            Credits = 3
-                    else:
-                        await interaction.followup.send("Error getting user's thumbnail.", ephemeral=True)
+                if not forceupdate:
+                    Debounce = True
+                    fetcher = AvatarFetcher(id, Username, interaction)
+                    await fetcher.run()
+                    asyncio.wait(60)
+                    Debounce = False
                 else:
-                    await interaction.followup.send("Request status code isn't 200 (Thumbnails API).", ephemeral=True)
+                    fetcher = AvatarFetcher(id, Username, interaction)
+                    async with aiohttp.ClientSession() as session:
+                        fetcher.fetch_batch_data(session, TokensTotal, True)
+                await interaction.followup.send("Finished.", ephemeral=True)
             else:
                 await interaction.followup.send("Username doesn't exist.", ephemeral=True)
         else:
