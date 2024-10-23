b6b053592c08df5802ab24a2eb5c83e8e4f0cf17
AndalYLP <andal089.az@gmail.com>
2024-10-23 13:48:59 -0500

    moved get_last_online to common file

--- RobloxPy/Presence.py
+++ RobloxPy/Presence.py
@@ -3,32 +3,11 @@ RobloxPy.Presence
 ~~~~~~~~~~~~~~~~~
 """
 
-from datetime import datetime
-
 from loguru import logger
 
+from ._common.presence import get_last_online
 from ._utils.classes import Presences, Users
 from ._utils.requests import _PresenceAPI
-
-
-def get_last_online(*userIds: int) -> dict[int, datetime]:
-    response = _PresenceAPI.V1.Presence.presence_last___online(*userIds)
-
-    responseJson: dict = response.json()
-    data: list = responseJson.get("lastOnlineTimestamps")
-
-    if data and "lastOnline" in data[0]:
-        result = {}
-        for value in data:
-            result[value["userId"]] = datetime.fromisoformat(
-                value["lastOnline"].replace("Z", "+00:00")
-            )
-
-        return result
-    else:
-        logger.exception(
-            KeyError(f"LastOnline not found in the response json", response.text)
-        )
 
 
 async def get_presence(*userIds: int) -> Presences.UserPresenceGroup:
