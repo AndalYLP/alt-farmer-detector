1152da34bc76ebefb8636d3b202ca547bcd7052b
AndalYLP <andal089.az@gmail.com>
2025-02-05 18:50:58 -0500

    moved purge command to its own file

--- src/commands/utils_commands/__init__.py
+++ src/commands/utils_commands/__init__.py
@@ -1,49 +1,7 @@
-import discord
-from discord import app_commands
 from discord.ext import commands
-from loguru import logger
 
-from config.command_description import UtilsDesc
-from config.embeds import error_embed
-from utils.exceptions import ProtectedCategory
-
-# ------------------------------------ Cog ----------------------------------- #
-
-
-class UtilsCommands(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-
-    mainGroup = app_commands.Group(name="utils", description="Utils commands")
-
-    @mainGroup.command(name="purge", description=UtilsDesc.purge)
-    @app_commands.describe(amount=UtilsDesc.amount)
-    async def purge_command(self, interaction: discord.Interaction, amount: int):
-        logger.log(
-            "COMMAND",
-            f"{interaction.user.name} used {interaction.command.name} command",
-        )
-
-        try:
-            if interaction.channel.category_id not in [
-                1290762586161414164,
-                1290715720321208462,
-                1298707633594961950,
-                1277035299528114187,
-            ]:
-                deleted = await interaction.channel.purge(limit=amount)
-
-                await interaction.response.send_message(
-                    content=f"purged {len(deleted)} message{"s" if len(deleted) > 1 else ""}."
-                )
-            else:
-                raise ProtectedCategory(
-                    interaction.channel.category.name, interaction.command.name
-                )
-        except Exception as e:
-            logger.exception(e)
-            await interaction.response.send_message(embed=error_embed(e))
+from commands.utils_commands.purge import PurgeCommand
 
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(UtilsCommands(bot))
+    await bot.add_cog(PurgeCommand(bot))
