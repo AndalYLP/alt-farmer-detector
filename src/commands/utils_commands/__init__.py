8bdce017531b1a4320432b094a09115cde72c1bc
AndalYLP <andal089.az@gmail.com>
2025-02-05 19:44:54 -0500

    removed local imports

--- src/commands/utils_commands/__init__.py
+++ src/commands/utils_commands/__init__.py
@@ -1,12 +1,7 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
-from loguru import logger
 
 from commands.utils_commands.purge import PurgeCommand
-from config.command_description import UtilsDesc
-from config.embeds import error_embed
-from utils.exceptions import ProtectedCategory
 
 
 async def setup(bot: commands.Bot):
