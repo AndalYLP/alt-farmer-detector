fe4265f5660924dea0e6117cf72f8c9abfd1afc7
AndalYLP <andal089.az@gmail.com>
2025-02-05 21:36:51 -0500

    trying a new organization method

--- src/commands/utils_commands/__init__.py
+++ src/commands/utils_commands/__init__.py
@@ -1,7 +1,27 @@
+from discord import app_commands
 from discord.ext import commands
 
+from commands.utils_commands import purge
 from commands.utils_commands.purge import PurgeCommand
 
 
+class create_utils_group(app_commands.Group):
+    def __init__(self):
+        super().__init__(name="utils", description="Utils commands")
+
+
+utils_group = create_utils_group()
+
+utils_group.add_command(purge)
+
+
+class utils_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def cog_load(self):
+        self.bot.tree.add_command(utils_group)
+
+
 async def setup(bot: commands.Bot):
-    await bot.add_cog(PurgeCommand(bot))
+    await bot.add_cog(utils_cog(bot))
--- src/commands/utils_commands/purge.py
+++ src/commands/utils_commands/purge.py
@@ -1,55 +1,20 @@
 import discord
 from discord import app_commands
-from discord.ext import commands
 from loguru import logger
 
 from config.command_description import UtilsDesc
 from config.embeds import error_embed
-from utils.categories import get_utils_group
 from utils.exceptions import ProtectedCategory
 
 
-class utils_group(app_commands.Group):
-    def __init__(self):
-        super().__init__(name="utils", description="Utils commands")
+@app_commands.command(name="purge", description=UtilsDesc.purge)
+@app_commands.describe(amount=UtilsDesc.amount)
+async def purge_command(interaction: discord.Interaction, amount: int):
+    logger.log(
+        "COMMAND",
+        f"{interaction.user.name} used {interaction.command.name} command",
+    )
 
-
-class PurgeCommand(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-
-    utils_group = utils_group()
-
-    @utils_group.command(name="purge", description=UtilsDesc.purge)
-    @app_commands.describe(amount=UtilsDesc.amount)
-    async def purge_command(self, interaction: discord.Interaction, amount: int):
-        logger.log(
-            "COMMAND",
-            f"{interaction.user.name} used {interaction.command.name} command",
-        )
-
-        try:
-            if interaction.channel.category_id not in [
-                1290762586161414164,
-                1290715720321208462,
-                1298707633594961950,
-                1277035299528114187,
-            ]:
-                deleted = await interaction.channel.purge(limit=amount)
-
-                await interaction.response.send_message(
-                    content=f"purged {len(deleted)} message{"s" if len(deleted) > 1 else ""}."
-                )
-            else:
-                raise ProtectedCategory(
-                    interaction.channel.category.name, interaction.command.name
-                )
-        except Exception as e:
-            logger.exception(e)
-            await interaction.response.send_message(embed=error_embed(e))
-
-
-async def purge_command(self, interaction: discord.Interaction, amount: int):
     try:
         if interaction.channel.category_id not in [
             1290762586161414164,
