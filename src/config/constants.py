e40d80a2ff48dd52b3ed7ae15b108c1a477233f2
AndalYLP <160054201+AndalYLP@users.noreply.github.com>
2025-02-27 11:34:47 -0500

    The ids should be type of `int`

--- src/config/constants.py
+++ src/config/constants.py
@@ -14,21 +14,22 @@ __data_base = MONGO_CLIENT["AltFarmerDetector"]
 
 USERS_COLLECTION = __data_base["Users"]
 
-GAME_ID = os.environ.get("GAME_ID") or os.getenv("GAME_ID")
+GAME_ID = int(os.environ.get("GAME_ID") or os.getenv("GAME_ID"))
 
-TRACKING_CATEGORY = os.environ.get("TRACKING_CATEGORY") or os.getenv(
-    "TRACKING_CATEGORY"
+TRACKING_CATEGORY = int(
+    os.environ.get("TRACKING_CATEGORY") or os.getenv("TRACKING_CATEGORY")
 )
 
-STATUS_CHANNEL_ID = os.environ.get("STATUS_CHANNEL_ID") or os.getenv(
-    "STATUS_CHANNEL_ID"
+STATUS_CHANNEL_ID = int(
+    os.environ.get("STATUS_CHANNEL_ID") or os.getenv("STATUS_CHANNEL_ID")
 )
-ALT_STATUS_CHANNEL_ID = os.environ.get("ALT_STATUS_CHANNEL_ID") or os.getenv(
-    "ALT_STATUS_CHANNEL_ID"
+ALT_STATUS_CHANNEL_ID = int(
+    os.environ.get("ALT_STATUS_CHANNEL_ID") or os.getenv("ALT_STATUS_CHANNEL_ID")
 )
-GAMEID_CHANNEL_ID = os.environ.get("GAMEID_CHANNEL_ID") or os.getenv(
-    "GAMEID_CHANNEL_ID"
+GAMEID_CHANNEL_ID = int(
+    os.environ.get("GAMEID_CHANNEL_ID") or os.getenv("GAMEID_CHANNEL_ID")
 )
-GAMEID_WITH_ALTS_CHANNEL_ID = os.environ.get(
-    "GAMEID_WITH_ALTS_CHANNEL_ID"
-) or os.getenv("GAMEID_WITH_ALTS_CHANNEL_ID")
+GAMEID_WITH_ALTS_CHANNEL_ID = int(
+    os.environ.get("GAMEID_WITH_ALTS_CHANNEL_ID")
+    or os.getenv("GAMEID_WITH_ALTS_CHANNEL_ID")
+)
