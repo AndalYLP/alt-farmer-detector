52a67ddd0f7913a520e963c0f8a72967c2536688
AndalYLP <160054201+AndalYLP@users.noreply.github.com>
2025-03-04 16:44:02 -0500

    Fixes #20

--- src/tracking.py
+++ src/tracking.py
@@ -78,7 +78,7 @@ async def manage_data_create_embed(
     time_in_gameid = user_game_info[1][1]
 
     presence_type_code = presenceTypeCode[
-        presence.userPresenceType if presence.lobbyStatus != "True" else "match"
+        presence.userPresenceType if presence.lobbyStatus == "True" else "match"
     ]
 
     color = presence_type_code[0]
